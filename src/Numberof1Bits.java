2b93a3d61b753551d124c509da7eaf3a0acf041e
michaelfeng <tennis_evil@hotmail.com>
2015-06-17 12:52:27 +0800

    implement Numberof1Bits more fundermental

--- src/Numberof1Bits.java
+++ src/Numberof1Bits.java
@@ -2,22 +2,24 @@ import core.Util;
 
 public class Numberof1Bits { // you need to treat n as an unsigned value
 	public int hammingWeight(int n) {
-		
-//		System.out.println(String.format("n:                                          %s",Integer.toBinaryString(n)));
-//		n = n - ((n >>> 1) & 0x55555555);
+		 
+		n =(n & 0x55555555) +  ((n>>1) & 0x55555555) ;
 //		System.out.println(String.format("n - ((n >>> 1) & 0x55555555):               %s",Integer.toBinaryString(n)));
-//		n = (n & 0x33333333) + ((n >>> 2) & 0x33333333);
+		
+		n = (n & 0x33333333) + ((n>>>2) & 0x33333333); 
 //		System.out.println(String.format("(n & 0x33333333) + ((n >>> 2) & 0x33333333):%s",Integer.toBinaryString(n)));
-//		n = (n + (n >>> 4)) & 0x0f0f0f0f;
+		
+		n = (n & 0x0f0f0f0f) + ((n>>>4) & 0x0f0f0f0f);
 //		System.out.println(String.format("(n + (n >>> 4)) & 0x0f0f0f0f:               %s",Integer.toBinaryString(n)));
-//		n = n + (n >>> 8);
+
+		n = (n & 0x00ff00ff) + ((n>>>8) & 0x00ff00ff);
 //		System.out.println(String.format("n + (n >>> 8):                              %s",Integer.toBinaryString(n)));
-//		n = n + (n >>> 16);
+
+		n = (n & 0x0000ffff) + ((n>>>16) & 0x0000ffff);
 //		System.out.println(String.format("n + (n >>> 16):                             %s",Integer.toBinaryString(n)));
-//		n= n & 0x3f;
-//		System.out.println(String.format("n & 0x3f:                                   %s",Integer.toBinaryString(n)));
-//		return n; 
-		return Integer.bitCount(n);
+		  
+		
+		return n;
 	}
 	
 //	public int hammingWeight(int n) {
@@ -34,16 +36,16 @@ public class Numberof1Bits { // you need to treat n as an unsigned value
 	public static void main(String[] args){
 		Numberof1Bits ins  = new Numberof1Bits();
 		int input  = 0;
-		Util.printInput(input);
-		Util.printResult(ins.hammingWeight(input), 0);
-		
-		input  = 1;
-		Util.printInput(input);
-		Util.printResult(ins.hammingWeight(input), 1);
-		
-		input  = 3;
-		Util.printInput(input);
-		Util.printResult(ins.hammingWeight(input), 2);
+//		Util.printInput(input);
+//		Util.printResult(ins.hammingWeight(input), 0);
+//		
+//		input  = 1;
+//		Util.printInput(input);
+//		Util.printResult(ins.hammingWeight(input), 1);
+//		
+//		input  = 3;
+//		Util.printInput(input);
+//		Util.printResult(ins.hammingWeight(input), 2);
 		
 		input  = 11;
 		Util.printInput(input);
